ed10b6fcf04106fdec7bb6cb7b961ead4d4008e0
Boris Poluianov <boris.poluianov@outlook.com>
2020-03-30 20:02:12 +0300

    feat(time_format_response): added response with time in requested format

--- app.rb
+++ app.rb
@@ -1,10 +1,24 @@
-class App
+require_relative 'time_handler'
 
+class App
   def call(env)
+    @request = Rack::Request.new(env)
+    if @request.path == '/time'
+      format_params = @request.params['format'].split(',')
+      time = TimeHandler.new(format_params)
+      response(time.status, time.response + "\n")
+    else
+      response(404, 'Not Found')
+    end
+  end
+
+  private
+
+  def response(code, text)
     [
-      200,
-      { 'Content-type' => 'text/plain' },
-      ["Welcome!\n"]
-     ]
-  end  
+      code,
+      { 'Content-Type' => 'text/plain' },
+      [text]
+    ]
+  end
 end
---
+++ time_handler.rb
@@ -0,0 +1,43 @@
+class TimeHandler
+  attr_reader :status, :response
+
+  VALID_FORMATS = {
+    'year' => '%Y-', 'month' => '%m-', 'day' => '%d',
+    'hour' => '%H:', 'minute' => '%M:', 'second' => '%S'
+  }.freeze
+
+  def initialize(params)
+    @valid = ''
+    @unvalid = []
+    @response = time_format(params)
+  end
+
+  private
+
+  def time_format(params)
+    parse(params)
+
+    return unvalid_format unless @unvalid.empty?
+    time_output
+  end
+
+  def unvalid_format
+    @status = 400
+    "Unvalid time format: #{@unvalid}"
+  end
+
+  def time_output
+    @status = 200
+    Time.now.strftime(@valid)
+  end
+
+  def parse(params)
+    params.each do |format|
+      if VALID_FORMATS[format]
+        @valid += VALID_FORMATS[format]
+      else
+        @unvalid << format
+      end
+    end
+  end
+end
